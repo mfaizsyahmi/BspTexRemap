753b685950dcd0b23000ee7915e78df21a7d0aaa
mfaizsyahmi <yaema.vandermerwe@gmail.com>
2024-07-04 16:59:34 +0800

    Update miptex.py
    - encode: fix writing wrong palette size
    - fix typo
    - fix wrong write size on palette #4 (water fog intensity)

--- src/jankbsp/types/miptex.py
+++ src/jankbsp/types/miptex.py
@@ -26,14 +26,14 @@ class MipTex:
     PALETTE_COLORS = 256
     PALETTE_SIZE = 768
     CP = "cp1252"
-    
+
     @classmethod
     def decode(cls, rawbytes):
         unpacked = MipTex.STRUCT.unpack_from(rawbytes,0)
-        # this _will_ contain junk after the string terminator, 
+        # this _will_ contain junk after the string terminator,
         # so must be partitioned. after that it must be encoded.
         name = unpacked[0].partition(b"\x00")[0].decode(MipTex.CP)
-        
+
         self = cls(name, *unpacked[1:3])
         self._rawname = unpacked[0]
 
@@ -45,11 +45,11 @@ class MipTex:
             mip_pos = unpacked[3+i]
             mip_len = self.width * self.height // (4**i)
             setattr(self, f"mip{i}", bytearray(rawbytes[mip_pos:mip_pos+mip_len]))
-            
+
             if i==3: # load palette at the end of mip3
                 pal_pos = mip_pos + mip_len + 2
                 self.color_count = int.from_bytes(
-                        rawbytes[mip_pos+mip_len:pal_pos], 
+                        rawbytes[mip_pos+mip_len:pal_pos],
                         byteorder='little'
                 )
                 self.palette = bytearray(rawbytes[pal_pos:pal_pos+MipTex.PALETTE_SIZE])
@@ -69,7 +69,7 @@ class MipTex:
         # truncate long names
         if len(header_parts[0]) > 15:
             header_parts[0] = header_parts[0][0:15] + b"\x00"
-        
+
         body = b""
         if not self.is_external:
             mip_offs = MipTex.STRUCT.size
@@ -77,8 +77,14 @@ class MipTex:
                 header_parts[3+i] = mip_offs + len(body)
                 body += self.get_mip(i) # get_mip generates mips if it doesn't exist
             # add a short with the color count, then the color palette (fixed size)
-            body += len(self.palette).to_bytes(2,byteorder='little') + self.palette
-            
+            body = b"\x00" * (MipTex.PALETTE_PAD + MipTex.PALETTE_SIZE)
+            body[0:MipTex.PALETTE_PAD] = int(len(self.palette) // 3)\
+                    .to_bytes(MipTex.PALETTE_PAD,byteorder='little')
+            pal = self.palette[0:MipTex.PALETTE_SIZE] \
+                    if len(self.palette) > MipTex.PALETTE_SIZE \
+                    else self.palette
+            body[MipTex.PALETTE_PAD:MipTex.PALETTE_PAD+len(pal)] = pal
+
         return MipTex.STRUCT.pack(*header_parts) + body
 
     @property
@@ -89,10 +95,10 @@ class MipTex:
         return MipTex.STRUCT.size if self.is_external \
         else MipTex.STRUCT.size + MipTex.PALETTE_PAD + MipTex.PALETTE_SIZE \
                 + int(len(self.mip0) * 1.328125)
-                
+
     @property
     def lname(self):
-        ''' returns name converted to lowercase for the sole purpose of 
+        ''' returns name converted to lowercase for the sole purpose of
             case-insensitive comparison
         '''
         return self.name.lower()
@@ -117,7 +123,7 @@ class MipTex:
         elif getattr(self,f"mip{level}") is not None:
             return getattr(self,f"mip{level}")
         # implicit else
-        
+
         # prepare the byte array of the target mip level
         values = bytearray(self.width*self.height/4**level)
         pos = 0
@@ -143,7 +149,7 @@ class MipTex:
         this.mip0 = b"".join(img.getdata())
         this.palette = b"".join(img.getpalette())
         return this
-    
+
     def to_image(self):
         if self.mip0 is None: return None
         img = Image.new("P", (self.width,self.height))
@@ -165,7 +171,7 @@ class MipTex:
         or not self.mip0 \
         or not b"\x03" in self.mip0 or not b"\x04" in self.mip0:
             return self
-        
+
         # quantize and get new mip and palette
         new_img = self.to_image().quantize(
                 colors=254, # 256 minus 2 fog indices
@@ -173,28 +179,28 @@ class MipTex:
         )
         mip = bytearray(new_img.getdata())
         pal = bytearray(new_img.getpalette())
-        
+
         # prepare values for palette #3 and #4
         if isinstance(fog_color, str):
             pal3 = bytes.fromhex(str)[0:3]
         elif isinstance(fog_color,bytes):
             pal3 = fog_color[0:3]
-        elif isinsntace(fog_color,Color):
+        elif isinstance(fog_color,Color):
             pal3 = fog_color.encode()
         else:
             pal3 = self.palette[9:12] # copy existing value
-        
+
         if fog_intensity:
-            pal4 = bytes(fog_intensity)
+            pal4 = bytes(fog_intensity) + b"\x00\x00"
         else:
             pal4 = self.palette[12:15] # copy existing value
-        
+
         # move over all entries and bitmap indices from #3 and #4
         pal = pal[0:9]+pal3+pal4+pal[9:]
         mip = bytearray(map(lambda x: x+2 if x>2 else x, mip))
-        
+
         # replace data, clear other mips
         self.palette = pal[0:256]
         self.mip0, self.mip1, self.mip2, self.mip3 = (mip,None,None,None)
-        
+
         return self # done
